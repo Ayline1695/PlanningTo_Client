28702773e53fc1f0ea6314a9cb4d176c510df85e
ayline <ayline1695@hotmail.com>
2021-03-10 16:32:28 +0100

    base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "./views/Lists/List";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Listas</h3>
+      <List />
+    </div>
+  );
+}
+
+export default App;
